f2e8a0b14e940d946b59cd285df39e64f98b70c9
SlawomirKedra <slaweczkowy@gmail.com>
2025-09-04 12:16:01 +0200

    Delete client/tailwind.config.cjs

--- client/tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      fontFamily: { display: ['Inter','ui-sans-serif','system-ui','sans-serif'] }
-    },
-  },
-  plugins: [],
-};
